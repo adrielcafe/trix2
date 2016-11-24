2d81fbac874d3b544c1f0ed2603873e643797c55
Misael Neto <misawsneto@gmail.com>
2016-11-24 18:30:09 -0200

    wordpress text success 4

--- src/main/java/trix2/config/multitenance/MultiTenantMongoDbFactory.java
+++ src/main/java/trix2/config/multitenance/MultiTenantMongoDbFactory.java
@@ -1,30 +1,30 @@
-package trix2.config.multitenance;
-
-import com.mongodb.DB;
-import com.mongodb.MongoClient;
-import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
-
-public class MultiTenantMongoDbFactory extends SimpleMongoDbFactory {
-
-    private String defaultDatabaseName;
-
-    public MultiTenantMongoDbFactory(MongoClient mongo, String database) {
-        super(mongo, database);
-        this.defaultDatabaseName = new String(database);
-		org.jcodec.common.logging.Logger.debug("Instantiating " + MultiTenantMongoDbFactory.class.getName() + " with " +
-				"default database name: " + defaultDatabaseName);
-    }
-
-    @Override
-    public DB getDb() {
-        final String subdomain = TenantContextHolder.getTenantSubdomain();
-        final String dbToUse;
-        if(subdomain == null)
-            dbToUse = defaultDatabaseName;
-        else
-            dbToUse = subdomain ;
-
-		org.jcodec.common.logging.Logger.debug("Acquiring database: " + dbToUse);
-        return super.getDb(dbToUse);
-    }
-}
+//package trix2.config.multitenance;
+//
+//import com.mongodb.DB;
+//import com.mongodb.MongoClient;
+//import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
+//
+//public class MultiTenantMongoDbFactory extends SimpleMongoDbFactory {
+//
+//    private String defaultDatabaseName;
+//
+//    public MultiTenantMongoDbFactory(MongoClient mongo, String database) {
+//        super(mongo, database);
+//        this.defaultDatabaseName = new String(database);
+//		org.jcodec.common.logging.Logger.debug("Instantiating " + MultiTenantMongoDbFactory.class.getName() + " with " +
+//				"default database name: " + defaultDatabaseName);
+//    }
+//
+//    @Override
+//    public DB getDb() {
+//        final String subdomain = TenantContextHolder.getTenantSubdomain();
+//        final String dbToUse;
+//        if(subdomain == null)
+//            dbToUse = defaultDatabaseName;
+//        else
+//            dbToUse = subdomain ;
+//
+//		org.jcodec.common.logging.Logger.debug("Acquiring database: " + dbToUse);
+//        return super.getDb(dbToUse);
+//    }
+//}
